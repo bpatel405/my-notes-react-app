d8a7f58e8c75a89f4ec4f4d4f5258d053b1ded0b
Brijesh Patel <b.patel405@mybvc.ca>
2019-03-01 19:33:27 -0700

    Updates

---
+++ precache-manifest.ff4e925c84763aa7d1a1f2550b85f9b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c1082ad19cf1e3e89d6",
+    "url": "/my-notes-react-app/static/js/runtime~main.2c1082ad.js"
+  },
+  {
+    "revision": "65d4407ff68538c1bf8e",
+    "url": "/my-notes-react-app/static/js/main.65d4407f.chunk.js"
+  },
+  {
+    "revision": "e0cfb1aa633d7a94fd00",
+    "url": "/my-notes-react-app/static/js/2.e0cfb1aa.chunk.js"
+  },
+  {
+    "revision": "65d4407ff68538c1bf8e",
+    "url": "/my-notes-react-app/static/css/main.ee036b88.chunk.css"
+  },
+  {
+    "revision": "e0cfb1aa633d7a94fd00",
+    "url": "/my-notes-react-app/static/css/2.ee80d9c3.chunk.css"
+  },
+  {
+    "revision": "0e4710eb089b66c5ec9b96f732ad2dcf",
+    "url": "/my-notes-react-app/index.html"
+  }
+];
